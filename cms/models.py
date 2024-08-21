5850a13b4aca98c7d325c564658152359657edd1
fred <fredpret@protonmail.ch>
2024-08-21 10:08:28 -0600

    added descriptions

--- cms/models.py
+++ cms/models.py
@@ -7,13 +7,18 @@ from django.urls import reverse
 
 # Create your models here.
 
+
 class SingletonModel(models.Model):
+    """
+    this class limits models to having a single instance
+    """
+
     class Meta:
         abstract = True
 
     def save(self, *args, **kwargs):
         if not self.pk and self.__class__.objects.exists():
-            raise ValidationError('There can be only one instance of this.')
+            raise ValidationError("There can be only one instance of this.")
         return super(SingletonModel, self).save(*args, **kwargs)
 
     @classmethod
@@ -44,41 +49,43 @@ class TeamMember(UserContent):
 
 class Article(UserContent):
     title = models.TextField()
-    teaser = models.CharField(blank=True,null=True, max_length=1000)
+    teaser = models.CharField(blank=True, null=True, max_length=1000)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     headline_image = models.ImageField(blank=True, null=True)
-    
+
     slug = models.SlugField(null=False, unique=True)
 
     class Meta:
-        verbose_name = 'Blog post'
-        verbose_name_plural = 'Blog posts'
+        verbose_name = "Blog post"
+        verbose_name_plural = "Blog posts"
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("cms:article_detail", kwargs={"slug": self.slug})
- 
+
 
 class Testimonials(UserContent, SingletonModel):
     class Meta:
-        verbose_name = 'Testimonials'
-        verbose_name_plural = 'Testimonials'
+        verbose_name = "Testimonials"
+        verbose_name_plural = "Testimonials"
 
 
 class Settings(models.Model):
-    dest_dir = models.CharField(max_length=255, help_text="Default directory for exported articles.")
+    dest_dir = models.CharField(
+        max_length=255, help_text="Default directory for exported articles."
+    )
     site_name = models.CharField(max_length=255, default="", help_text="Site name")
     site_headline = models.CharField(max_length=255, default="", help_text="Site headline")
     site_teaser = models.CharField(max_length=255, default="", help_text="Site teaser")
     about = QuillField()
 
     class Meta:
-        verbose_name = 'Settings'
-        verbose_name_plural = 'Settings'
-    
+        verbose_name = "Settings"
+        verbose_name_plural = "Settings"
+
     def clean(self):
         # Ensure only one instance exists
         if Settings.objects.exclude(pk=self.pk).exists():
