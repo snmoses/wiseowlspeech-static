863ffd363dece9d60bfa4c1564a39516601d7e38
Sam <snmoses35@gmail.com>
2026-07-23 18:41:46 -0600

    Fix GitHub Pages asset paths

--- django_static/settings.py
+++ django_static/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = os.getenv("DJANGO_STATIC_URL", "/static/")
 STATICFILES_DIRS = [
     static_dir
     for static_dir in [os.path.join(BASE_DIR, "static")]
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media files (Uploaded files)
-MEDIA_URL = "/media/"
+MEDIA_URL = os.getenv("DJANGO_MEDIA_URL", "/media/")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
